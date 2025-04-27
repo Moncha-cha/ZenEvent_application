82006fb55b9732db9224a38d35180f044bcfa396
Monika Lochmanova <monikalochmanova1@gmail.com>
2025-04-27 14:01:19 +0200

    Navigation, main container

--- zenevent/src/App.js
+++ zenevent/src/App.js
@@ -6,9 +6,9 @@ import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 import EventLink from './components/EventLink';
-import Logo from './components/Logo';
-import Navigation from './components/HeaderText';
 import Header from './components/Header';
+import Navigation from './components/Navigation';
+
 
 
 
@@ -16,57 +16,18 @@ function App() {
 
 
   return (
-   <>
-  <Header/> {/* Obsahuje <Logo/> a <Navigation/> */}
-
-
-
-
-    <nav className='navbar navbar-expand-lg bg-light'>
-
-      <button 
-      className='navbar-toggler ms-auto'
-      type='button'
-      data-bs-toggle='collapse'
-      data-bs-target='#navbarNav'
-      aria-controls='navbarNav'
-      aria-expanded='false'
-      aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon'></span>
-      </button>
-
-      <div className='collapse navbar-collapse' id='navbarNav'>
-
-        <ul className='navbar-nav custom-nav'>
-          <li className='nav-item'>
-            <a className='nav-link active' aria-current='page' href='#udalosti'>Události</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#navod'>Návod</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#tipy'>Tipy</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#ome'>O mě</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#kontakt'>Kontakt</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-
-
-  
+    <>
+      <Header /> {/* Obsahuje <Logo/> a <Navigation/> */}
+      <Navigation />
       <EventLink />
-      <MainContent />
-      <ContactForm />
+
+      <main className='container'>
+        <MainContent />
+        <ContactForm />
+      </main>
+
       <Footer />
-  
-   </>
+    </>
   );
 }
 
---
+++ zenevent/src/components/Navigation.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav className='navbar navbar-expand-lg bg-light'>
+
+      <button 
+      className='navbar-toggler ms-auto'
+      type='button'
+      data-bs-toggle='collapse'
+      data-bs-target='#navbarNav'
+      aria-controls='navbarNav'
+      aria-expanded='false'
+      aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+
+      <div className='collapse navbar-collapse' id='navbarNav'>
+
+        <ul className='navbar-nav custom-nav'>
+          <li className='nav-item'>
+            <a className='nav-link active' aria-current='page' href='#udalosti'>Události</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#navod'>Návod</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#tipy'>Tipy</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#ome'>O mě</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#kontakt'>Kontakt</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    )
+}
